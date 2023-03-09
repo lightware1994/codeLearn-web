5638acd5060f66f0bea0401fbafd81d54ead1df5
Douglasfj <douglasfj2004@gmail.com>
2023-03-08 21:14:51 -0300

    arrumando problema das imagens

--- js/verificacaoUnidade.js
+++ js/verificacaoUnidade.js
@@ -1,61 +1,30 @@
-//Pegando o formulário
-const frm = document.querySelector("form")  
 
-//Pegando o img
-const imUnidade= document.querySelector("#imgUnidade")
+var checkTaguatinga = document.getElementById("taguatinga")
+var checkBrasilia = document.getElementById("brasilia")
+var checkGama = document.getElementById("gama")
+var checkSobradinho = document.getElementById("sobradinho")
 
-//Pegando o título
-const dvTitulo = document.querySelector("#divTitulo")
+img = document.getElementById("imagem")
 
+radios = document.getElementsByName("radio-name")
 
-//Função para trocar de unidade
-const trocarUnidade= () => {
-  let unidade                           // variável que irá receber o nome do clube
+for (let i=0; i<4; i++){
+    radios[i].addEventListener("change", ()=>{
+        let unidade
 
-  if (frm.rbBrasilia.checked) {         // verifica qual radiobutton está selecionado
-    unidade = "Brasilia"
-  } else if (frm.rbSobradinho.checked) {
-    unidade = "Sobradinho"
-  } else if(frm.rbGama.checked){
-    unidade = "Gama"
-  }else{
-    unidade ="Taguatinga"
-  }
+        if (checkBrasilia.checked) {
+            unidade = "senaiBrasília"
+        } else if (checkSobradinho.checked) {
+            unidade = "senaiSobradinho"
+        } else if(checkGama.checked){
+            unidade = "senaiGama"
+        }else{
+            unidade ="taguatinga"
+        }
 
-  // define as classes de dvTitulo: row e cores da unidade
-  dvTitulo.className = `row cores-${unidade}`
+        img.src = `/img/${unidade}.png`
+        localStorage.setItem("unidade", unidade)
 
-  // modifica a imagem de acordo com a seleção do cliente
-  imUnidade.src = `img/${unidade.toLowerCase()}.jpg`
-  imUnidade.className = "img-fluid"        // muda o estilo para exibir a imagem
-  imUnidade.alt = `Símbolo do ${unidade}`    // modifica atributo alt
-
-  localStorage.setItem("unidade", unidade)   // salva no navegador a escolha do cliente
+        localStorage.setItem("unidade", unidade)
+    })
 }
-
-// associa ao evento change de cada botão do form a função trocarUnidade  
-frm.rbBrasilia.addEventListener("change", trocarUnidade)   
-frm.rbSobradinho.addEventListener("change", trocarUnidade)   
-frm.rbTaguatinga.addEventListener("change", trocarUnidade) 
-frm.rbGama.addEventListener("change", trocarUnidade) 
-
-
-//Função vai usar o getItem para verificar a unidade
-const verificarUnidade = () => {
-  if (localStorage.getItem("unidade")) {           // se já estiver salvo algum clube
-    const unidade = localStorage.getItem("unidade")  // obtém o nome do clube
-
-    if (unidade == "Brasilia") {                     // conforme o clube, marca checked 
-      frm.rbBrasilia.checked = true
-    } else if (unidade == "Sobradinho") {
-      frm.rbSobradinho.checked = true
-    } else if(unidade = "Gama"){
-      frm.rbGama.checked = true
-    }else{
-      frm.rbTaguatinga.checked = true
-    }
-    trocarUnidade()                          // chama function que troca imagem e cores
-  }
-}
-// ao carregar a página, verifica se cliente já selecionou unidade anteriormente
-window.addEventListener("load", verificarUnidade)
